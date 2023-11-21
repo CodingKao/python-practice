c95d160520c76afd2520aec114ebe31888dea65c
Kao Saelor <saelor.kao@gmail.com>
2023-11-20 20:41:38 -0800

    boolean to string

---
+++ booleanToString.py
@@ -0,0 +1,10 @@
+# Implement a function which convert the given boolean value into its string representation.
+
+# Defind the function
+def boolean_to_string(b):
+    return str(b)
+
+# Example usage:
+boolean_value = True
+string_representation = boolean_to_string(boolean_value)
+print(string_representation)
