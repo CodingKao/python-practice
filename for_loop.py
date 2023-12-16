fb0c4bd5647f52a83008137f98f19d1d7bd40209
Kao Saelor <saelor.kao@gmail.com>
2023-12-16 15:49:53 -0800

    for loop with skip

--- for_loop.py
+++ for_loop.py
@@ -1,6 +1,11 @@
 # Practice for loop
 
+print("Iterate 1 - 5")
 # Using range() to iterate from 1 to 5
 for number in range(1,6):
     print(number)
-
+   
+print("Iterate 1-10 with a skip of 2")
+# Add skip
+for number in range(1,10,2):
+    print(number)
