840dd1f4812d619533a30d690f2f8f69d4f1a011
Kao Saelor <saelor.kao@gmail.com>
2023-12-16 05:05:23 -0800

    Gross rent multiplier

--- gross_rent_multiplier.py
+++ gross_rent_multiplier.py
@@ -3,14 +3,20 @@
 # The Gross Rent Multiplier is often used as a quick and rough measure to assess the relative value of different investment properties. A lower GRM generally suggests that the property may be a better value because it indicates a shorter time for the property to pay for itself based on the rental income.
 
 # Define the Gross Rent Multiplier function
+def grm(property_price,gross_rental_income):
+    # calculate grm
+    grm = property_price / gross_rental_income
+    return grm
 
 
 # # Ask user for input
-
+property_price = float(input("Enter a property price: "))
+gross_rental_income = float(input("Enter the gross rental income: "))
 
 
 # # calculate GRM
-
+result = grm(property_price,gross_rental_income)
 
 
 # Print result
+print(f"THe Gross Rent Multiplier (GRM) is: {result:.2f}")
