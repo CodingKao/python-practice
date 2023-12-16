0ca4e3d4a7ad32f623561538b31f9093da411d1a
Kao Saelor <saelor.kao@gmail.com>
2023-12-16 01:20:14 -0800

    cash on cash return

--- cash_on_cash_return.py
+++ cash_on_cash_return.py
@@ -3,13 +3,17 @@
 # Cash-on-cash return = (Net Operating Income / Total Cash Invested) * 100
 
 # define the function
-
+def calculate_cash_on_cash_return(noi, total_cash_invested):
+    # calculate cash-on-cash return
+    cash_on_cash_return = (noi / total_cash_invested) *100
+    return cash_on_cash_return
 
 # ask user for input
-
-
+noi = float(input("Enter the Net Operating Income: "))
+total_cash_invested = float(input("Enter the Total Cash Invested: "))
 
 # calculate the cash-on-cash return
-
+result = calculate_cash_on_cash_return(noi, total_cash_invested)
 
 # print result
+print(f"The cash-on-cash return is: {result:.2f}%")
