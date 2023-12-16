5fbac3b4c0f9813218cdbc2bc10facc9f9846a39
Kao Saelor <saelor.kao@gmail.com>
2023-12-16 01:06:26 -0800

    cash on cash return steup

---
+++ cash_on_cash_return.py
@@ -0,0 +1,15 @@
+# Cash-on-Cash Return is a financial metric used in real estate investing to evaluate the return on actual cash invested in a property.
+# It's expressed as a percentage and provides insight into the cash flow performance of an investment relative to the amount of cash initially invested. The formula for calculating Cash-on-Cash Return is as follows:
+# Cash-on-cash return = (Net Operating Income / Total Cash Invested) * 100
+
+# define the function
+
+
+# ask user for input
+
+
+
+# calculate the cash-on-cash return
+
+
+# print result
