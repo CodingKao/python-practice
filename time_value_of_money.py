32e8bda52af61802f146a6f42ad558dd7a49aac6
Kao Saelor <saelor.kao@gmail.com>
2023-12-16 11:21:14 -0800

    time value of money

--- time_value_of_money.py
+++ time_value_of_money.py
@@ -9,3 +9,31 @@
 # FV is the future value,
 # r is the interest rate per period,
 # n is the number of periods
+
+# Define the present value function
+def calculate_present_value(future_value, interest_rate, period):
+    present_value = future_value / (1 + interest_rate) ** period
+    return present_value
+
+# Define the future value function
+def calculate_future_value(present_value, interest_rate, period):
+    future_value = present_value * (1+ interest_rate) ** period
+
+
+# Ask user for input
+future_value = float(input("Enter the future value: "))
+present_value = float(input("Enter the present value: "))
+interest_rate = float(input("Enter the interest rate: "))
+period = float(input("Enter the period: "))
+
+
+# calculate the present value
+present_value = calculate_present_value(future_value, interest_rate, period)
+
+print(f"The Present Value is: ${present_value:.2f}")
+
+
+# calculate future value
+future_value = calculate_future_value(future_value, interest_rate, period)
+
+print(f"The Future Value is: ${future_value:.2f}")
