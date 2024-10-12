101bd61d599cb677d321a5ebd65e898349825d46
Kao Saelor <saelor.kao@gmail.com>
2024-10-12 03:52:08 -0700

    string to letters

--- string_to_array.py
+++ string_to_array.py
@@ -10,3 +10,11 @@ input_string = input("Enter a string: ")
 # Call the function and print result
 word_array = string_to_array(input_string, ",")
 print(word_array)
+
+# Write a function to split a string of words by letter
+def string_to_letters(s):
+    return list(s)
+
+# call the function and print result
+letter_array = string_to_letters(input_string)
+print(letter_array)
