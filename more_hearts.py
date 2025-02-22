495ce797c231ca897370a9772780272dbea54de7
Kao Saelor <saelor.kao@gmail.com>
2025-02-22 14:35:10 -0800

    more hearts

---
+++ more_hearts.py
@@ -0,0 +1,7 @@
+# Write a function that returns the # of hearts based on user input
+
+# Ask the user for the number of hearts
+num_hearts = int(input("How many hearts do you want?"))
+
+# Print the heart symbol the sepcified number of tiems
+print("❤️  " * num_hearts)
